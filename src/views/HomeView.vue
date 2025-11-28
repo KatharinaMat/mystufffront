6c0d6ce18028d40ef807a825a803b3ae986be686
KatharinaMat <katharina.matkevits@gmail.com>
2025-11-28 16:49:25 +0200

    HomeView adjustments

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -7,14 +7,35 @@
   <div class="container text-center">
     <div class="row">
       <div>
-      <nav class="d-flex justify-content-center gap-6">
-        <router-link to="/login">Login</router-link> |
-        <router-link to="/new-account">Create account</router-link>
+      <nav class="auth-links">
+        <h3><router-link to="/login">Login</router-link>  </h3>
+        <h3><router-link to="/new-account"> Create account</router-link></h3>
       </nav>
-    </div>
+        <img src="@/assets/background_picture.webp" alt="background" class="home-image">
+
+      </div>
   </div>
   </div>
 </template>
+
+<style scoped>
+h1 {
+  margin-top: 60px; /* to push the h1 lower */
+  margin-bottom: 40px;
+}
+  .auth-links {
+    display: flex;
+    justify-content: center;
+    gap: 40px;
+  }
+.home-image {
+  display: block;
+  margin: 40px auto 0 auto; /* top margin + centered */
+  max-width: 400px;         /* prevents the picture from getting too big */
+  height: auto;             /* keeps proportions */
+}
+
+</style>
 
 <script>
 export default {
