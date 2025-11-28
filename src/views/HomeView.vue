756c7cc39b70c17c64ba3d6d606a6084628caedc
Hanna <hanna.kuusvere@gmail.com>
2025-11-28 15:14:01 +0200

    Homeview WIP

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,23 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container text-center">
+    <div>
+      <h1>MyStuffLabelled</h1>
+    </div>
+  </div>
+  <div class="container text-center">
+    <div class="row">
+      <div>
+      <nav class="d-flex justify-content-center gap-6">
+        <router-link to="/login">Login</router-link> |
+        <router-link to="/new-account">Create account</router-link>
+      </nav>
+    </div>
+  </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
+  name: 'HomeView'
 }
 </script>
