4415fbf5e64f5d43b75f0afc0fc94fef9bbc8630
KatharinaMat <katharina.matkevits@gmail.com>
2025-12-01 16:23:51 +0200

    started loginView

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -6,18 +6,11 @@
   </div>
   <div class="container text-center">
     <div class="row">
-      <div>
         <nav class="home-links">
           <h3>
             <router-link to="/login">Login</router-link>
           </h3>
-          <h3>
-            <router-link to="/new-account"> Create account</router-link>
-          </h3>
         </nav>
-
-
-      </div>
     </div>
   </div>
 </template>
@@ -27,13 +20,6 @@ h1 {
   margin-top: 30px; /* to push the h1 lower */
   margin-bottom: 40px; /*to push the next block lower*/
 }
-
-.home-links {
-  display: flex; /*Makes the children ("LogIn" & "Create account") sit next to each other horizontally by default.*/
-  justify-content: center;
-  gap: 40px; /*gap between links*/
-}
-
 
 
 </style>
